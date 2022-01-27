e1ce99c1e06ed4d655e6befcd3844bf878374ba6
Rikki Mount <rmount@F2K010.42adel.org.au>
2022-01-27 15:54:21 +1030

    Add libfts

---
+++ libft.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmount <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 10:40:42 by rmount            #+#    #+#             */
+/*   Updated: 2022/01/27 15:36:25 by rmount           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include<stdlib.h>
+# include<unistd.h>
+
+/* CHARACTER FUNCS */
+
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+
+
+/* CONVERSION FUNCS */
+
+
+/* LIST FUNCS */
+
+
+/* MEMORY FUNCS */
+
+
+/* PRINT FUNCS */
+
+
+/* STRING FUNCS */
+size_t	ft_strlen(const char *s);
+
+#endif
