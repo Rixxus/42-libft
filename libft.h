a7eeea1e78b35f3a610bb1b9cb0f74d8414b1a83
Rikki Mount <rmount@F3C017.42adel.org.au>
2022-01-28 16:59:14 +1030

    Modded libs

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rmount <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 10:40:42 by rmount            #+#    #+#             */
-/*   Updated: 2022/01/28 13:45:54 by rmount           ###   ########.fr       */
+/*   Updated: 2022/01/28 15:51:40 by rmount           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,10 @@ int	ft_isdigit(int c);
 
 
 /* STRING FUNCS */
+char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	ft_tolower(int c);
 char	ft_toupper(int c);
 
 #endif
