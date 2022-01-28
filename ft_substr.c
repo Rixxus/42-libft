dd9ee3416bc200915b7cadee84a7caeb8b32fb3a
Rikki Mount <rmount@F3C017.42adel.org.au>
2022-01-28 17:00:49 +1030

    Add substr

---
+++ ft_substr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmount <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/28 15:24:35 by rmount            #+#    #+#             */
+/*   Updated: 2022/01/28 15:29:12 by rmount           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*result;
+	unsigned int	i;
+
+	i = 0;
+	result = (char *)malloc(sizeof(char) * len + 1);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len && s[start + i])
+	{
+		result[i] = s[start + i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
