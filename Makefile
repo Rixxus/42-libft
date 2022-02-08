b9d74e925f374f8fc4a1dd116fae56063a2cacac
Rikki Mount <rmount@F2K010.42adel.org.au>
2022-02-08 15:28:52 +1030

    Update Makefile & libft

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: rmount <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/01/28 14:49:46 by rmount            #+#    #+#              #
-#    Updated: 2022/02/03 11:09:54 by rmount           ###   ########.fr        #
+#    Updated: 2022/02/08 15:22:50 by rmount           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,9 +24,10 @@ SRC_CHAR = ft_isalnum.c \
 		   ft_isdigit.c \
 		   ft_isprint.c
 
-SRC_CONVERSION = 
+SRC_CONVERSION = ft_atoi.c 
 
 SRC_MEMORY = ft_bzero.c \
+			 ft_calloc.c \
 			 ft_memchr.c \
 			 ft_memcmp.c \
 			 ft_memcpy.c \
@@ -36,10 +37,13 @@ SRC_MEMORY = ft_bzero.c \
 SRC_PRINT = 
 
 SRC_STR = ft_strchr.c \
+		  ft_strdup.c \
+		  ft_strncmp.c \
 		  ft_strjoin.c \
 		  ft_strlcat.c \
 		  ft_strlcpy.c \
 		  ft_strlen.c \
+		  ft_strnstr.c \
 		  ft_strrchr.c \
 		  ft_strtrim.c \
 		  ft_substr.c \
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rmount <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 10:40:42 by rmount            #+#    #+#             */
-/*   Updated: 2022/02/03 11:11:00 by rmount           ###   ########.fr       */
+/*   Updated: 2022/02/08 15:23:51 by rmount           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,14 @@ int		ft_isprint(int c);
 
 
 /* CONVERSION FUNCS */
-
+int		ft_atoi(const char *str);
 
 /* LIST FUNCS */
 
 
 /* MEMORY FUNCS */
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -43,10 +44,13 @@ void	*ft_memset(void *b, int c, size_t len);
 
 /* STRING FUNCS */
 char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
