c6bf0143b8f354c9822fe7d525940774c1c0d82b
Rikki Mount <rmount@F2N060.42adel.org.au>
2022-02-15 09:29:52 +1030

    Mod putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rmount <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 15:26:28 by rmount            #+#    #+#             */
-/*   Updated: 2022/02/11 16:24:33 by rmount           ###   ########.fr       */
+/*   Updated: 2022/02/15 09:28:53 by rmount           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,24 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*string;
-
-	string = ft_itoa(n);
-	ft_putstr_fd(string, fd);
-	free(string);
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	if (n < 10)
+	{
+		ft_putchar_fd(n + 48, fd);
+	}	
 }
